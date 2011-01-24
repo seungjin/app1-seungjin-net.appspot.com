41b6fe06ff0f1f9c853f59c5b78a1676845174a8
seungjin <seungjin@seungjin.net>
2011-01-24 00:03:00 -0700

    .

--- module/model_classes.rb
+++ module/model_classes.rb
@@ -1,8 +1,8 @@
 
 # Create your model class
+
 class Shout
   include DataMapper::Resource
-  
   property :id, Serial
   property :message, Text
   property :created_at, DateTime, :default => Time.now
